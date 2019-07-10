637c48402d863a87f5b6230736e8b4c4bd07344b
workingjubilee <workingjubilee@gmail.com>
2019-07-10 15:43:49 -0700

    Completed blockchain node

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -6,6 +6,7 @@ from uuid import uuid4
 from flask import Flask, jsonify, request
 
 import sys
+# from collections import OrderedDict?
 
 
 class Blockchain(object):
@@ -14,7 +15,7 @@ class Blockchain(object):
         self.current_transactions = []
         self.nodes = set()
 
-        self.new_block(previous_hash=1, proof=100)
+        self.new_block(previous_hash=1, proof=99)
 
     def new_block(self, proof, previous_hash=None):
         """
@@ -57,25 +58,6 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
-    @staticmethod
-    def hash(block):
-        """
-        Creates a SHA-256 hash of a Block
-
-        :param block": <dict> Block
-        "return": <str>
-        """
-
-        # We must make sure that the Dictionary is Ordered,
-        # or we'll have inconsistent hashes
-
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
-
-    @property
-    def last_block(self):
-        return self.chain[-1]
-
     def proof_of_work(self, last_proof):
         """
         Simple Proof of Work Algorithm
@@ -91,6 +73,32 @@ class Blockchain(object):
         return proof
 
     @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a Block
+
+        :param block": <dict> Block
+        "return": <str>
+        """
+
+        # We must make sure that the Dictionary is Ordered,
+        # or we'll have inconsistent hashes
+
+        # ordict = OrderedDict() ?
+
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def last_proof(self):
+        last = self.last_block()
+        return last.proof
+        # return last_proof
+
+    @staticmethod
     def valid_proof(last_proof, proof):
         """
         Validates the Proof:  Does hash(last_proof, proof) contain 4
@@ -98,7 +106,7 @@ class Blockchain(object):
         """
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"
+        return guess_hash[:6] == "000000"
 
     def valid_chain(self, chain):
         """
@@ -141,32 +149,48 @@ blockchain = Blockchain()
 
 
 @app.route('/mine', methods=['GET'])
-def mine():
+def mine(proof):
     # We run the proof of work algorithm to get the next proof...
     last_block = blockchain.last_block
     last_proof = last_block['proof']
-    proof = blockchain.proof_of_work(last_proof)
-
+    if blockchain.valid_proof(proof, last_proof):
     # We must receive a reward for finding the proof.
     # The sender is "0" to signify that this node has mine a new coin
-    blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifier,
-        amount=1,
-    )
+        blockchain.new_transaction(
+            sender="0",
+            recipient=node_identifier,
+            amount=1,
+        )
 
-    # Forge the new BLock by adding it to the chain
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(proof, previous_hash)
+    # Forge the new Block by adding it to the chain
+        previous_hash = blockchain.hash(last_block)
+        block = blockchain.new_block(proof, previous_hash)
 
+        response = {
+            'message': "New Block Forged",
+            'index': block['index'],
+            'transactions': block['transactions'],
+            'proof': block['proof'],
+            'previous_hash': block['previous_hash'],
+        }
+        return jsonify(response), 200
+    else:
+        response = {
+            'message': 'Nah bud this ain\'t it, try hashing with this:'
+            'last_proof': last_proof
+        }
+
+        return jsonify(response), 403
+
+@app.route('/last_proof', methods=['GET'])
+def reply_with_proof(proof):
+    last_proof = blockchain.last_proof()
     response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
+        'last_proof': last_proof
     }
+    
     return jsonify(response), 200
+
 
 
 @app.route('/transactions/new', methods=['POST'])
